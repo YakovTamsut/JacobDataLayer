9ef5afa84429286bd01d5fbe93ced8261181746d
YakovTamsut <yakov.t06@gmail.com>
2024-05-08 11:19:06 +0300

    Add project files.

---
+++ Host/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml;
+using Model;
+using ServiceModel;
+using ViewModel;
+
+namespace Host
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            ServiceHost service = new ServiceHost(typeof(BaseServiceModel));
+            service.Open();
+            //LoadPictures load = new LoadPictures();
+            //load.LoadExercises();
+        }
+
+       
+    }
+}
